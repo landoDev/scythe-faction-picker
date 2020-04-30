dca3f892714e604037bde691132b18556217a6ce
Landon Turner <lturner@teamworks.com>
2020-04-30 17:11:37 -0400

    configures knexfile for development

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,20 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './data/scythe_cm.db3'
+    },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done); // foreign key enforcement for sqlite3
+      },
+    },
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
   },
 
   staging: {
