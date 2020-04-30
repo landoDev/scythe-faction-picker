51629cfabb19ac94bd6075cbb1f3c2e672ca95e6
Landon Turner <lturner@teamworks.com>
2020-04-30 17:17:50 -0400

    adds campaign router. endpoints all return null

--- api/server.js
+++ api/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const campaignRouter = require('../campaigns/campaigns-router');
 
 const server = express();
 
-server.use(express.json())
+server.use(express.json());
 
-server.get('/', (req,res) => {
+server.use("/api/campaigns", campaignRouter);
+
+server.get('/', (req, res) => {
     res.send(`<h1>Scythe Campaign Manager API</h1>`)
 })
 
---
+++ campaigns/campaigns-router.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.router();
+
+router.get('/', (req, res) => {
+    return null;
+});
+
+router.get('/:id', (req, res) => {
+    return null;
+});
+
+router.post('/', (req, res) => {
+    return null;
+});
+
+router.put('/:id', (req, res) => {
+    return null;
+});
+
+router.delete('/:id', (req, res) => {
+    return null;
+});
