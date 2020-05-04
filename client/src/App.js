6ee63a4ec99e56d19c2e29d5ddaa907886a12817
Landon Turner <lturner@teamworks.com>
2020-05-04 12:48:35 -0400

    initial commit for new components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
+
+// Component Imports
 import QuickFactionForm from './components/QuickFactionForm';
+import CreateCampaignForm from './components/CreateCampaignForm'
 // import { Button, Spinner } from 'reactstrap';
 import './App.css';
 
@@ -12,6 +15,7 @@ function App() {
         <h1 className='title'>Scythe Faction Selector</h1>
       </header>
       <QuickFactionForm />
+      <CreateCampaignForm />
       <footer className='signature'>Developed by Landon Turner</footer>
     </div>
   );
---
+++ client/src/components/CreateCampaignForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const CreateCampaignForm = props => {
+    return (
+        <div>
+            <h2>Create Campaign</h2>
+        </div>
+    )
+}
+
+export default CreateCampaignForm;
