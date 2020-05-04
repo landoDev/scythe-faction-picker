486d19647a2b05b865d52d6843e648cf15472972
Landon Turner <lturner@teamworks.com>
2020-05-04 14:27:06 -0400

    adds some functions to create campaign

--- client/src/components/CreateCampaignForm.js
+++ client/src/components/CreateCampaignForm.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'reactstrap';
 
 const CreateCampaignForm = props => {
+    const [newCampaign, setNewCampaign] = {
+        code: '',
+        created: null
+    }
+    const handleChanges = (e) => {
+        setNewCampaign({
+          ...newCampaign,
+          [e.target.name]: e.target.value
+        });
+      };
+
+    // can uncomment and assign after you connect component to store
+    // const onSubmit = (e) => {
+    //     e.preventDefault();
+    //     let utc = new Date().toJSON().slice(0,10).replace(/-/g,'/');
+    //     newCampaign.created = utc;
+    //     props.createCampaign(newCampaign);
+    // }
+
     return (
         <div>
             <h2>New Campaign</h2>
             <p>Give your Campaign a code, you will need it to find it later</p>
             <form>
                 <label>Code:</label>
-                <input type='text' />
-                <Button color='primary'>Create</Button>
+                <input type='text' name='code' />
+                <Button color='primary' type='submit'>Create</Button>
             </form>
         </div>
     )
