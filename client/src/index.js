874c08f996fa7dd8ad0e7787abcbb43d70724608
Landon Turner <lturner@teamworks.com>
2020-05-04 12:04:35 -0400

    configures redux, adds thunk dependency

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import campaignReducer from './reducers';
+
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(campaignReducer);
+
 ReactDOM.render(
   <React.StrictMode>
+  <Provider store={store}>
     <App />
+  </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
