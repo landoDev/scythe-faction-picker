71b5415a1aa17faccee26841340aa1aacde8e00f
Landon Turner <lturner@teamworks.com>
2020-05-04 16:27:50 -0400

    remove redundant cors middleware

--- middleware/index.js
+++ middleware/index.js
@@ -47,8 +47,3 @@ function verifyPlayerBody(req, res, next) {
       }
 };
 
-function corsProxy(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-}
