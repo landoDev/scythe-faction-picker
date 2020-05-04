2a75336ac34ad1afa4cfd6f72e6ebfcc4dcbc944
Landon Turner <lturner@teamworks.com>
2020-05-04 18:46:12 -0400

    server fix, adds handle changes

--- client/src/components/CreateCampaignForm.js
+++ client/src/components/CreateCampaignForm.js
@@ -23,25 +23,22 @@ const CreateCampaignForm = props => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        console.log('submit starts')
         let utc = new Date().toJSON().slice(0,10).replace(/-/g,'/');
         newCampaign.created = utc;
+        setNewCampaign(newCampaign)
+        console.log('new campaign', newCampaign)
         props.createCampaign(newCampaign);
-        setNewCampaign(
-            {
-                code: '',
-                created: null
-            }
-        )
         // history.push('/campaign/add-players');
     }
-
+    console.log('check changes', newCampaign)
     return (
         <div>
             <h2>New Campaign</h2>
             <p>Give your Campaign a code, you will need it to find it later</p>
             <form onSubmit={handleSubmit}>
                 <label>Code:</label>
-                <input type='text' name='code' />
+                <input type='text' name='code' onChange={handleChanges} />
                 {props.isPosting ? <Spinner className='add-btn' color='warning' />
                 : <Button color='primary' type='submit'>Create</Button>}
             </form>
