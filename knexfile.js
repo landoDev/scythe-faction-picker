f75fe575c27e4a9eb9a2a35cc4f9c6bb4ac77e65
Landon Turner <lturner@teamworks.com>
2020-05-07 18:37:42 -0400

    postgres prod setup

--- knexfile.js
+++ knexfile.js
@@ -39,20 +39,16 @@ module.exports = {
   //   }
   // },
 
-//   production: {
-//     client: 'postgresql',
-//     connection: {
-//       database: 'my_db',
-//       user:     'username',
-//       password: 'password'
-//     },
-//     pool: {
-//       min: 2,
-//       max: 10
-//     },
-//     migrations: {
-//       tableName: 'knex_migrations'
-//     }
-//   }
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations',
+    },
+    // no seeds yet, not really necessary
+    // seeds: {
+    //   directory: './data/seeds',
+    // },
+  }
 
 };
